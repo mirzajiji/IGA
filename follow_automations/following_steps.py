6b3daa839fb5d0e64f3fa1437f142a902eb2cbd9
Mirza ROG <mirzajijieshvili@gmail.com>
2024-05-17 14:14:50 +0400

    fix freeze side

--- follow_automations/following_steps.py
+++ follow_automations/following_steps.py
@@ -27,6 +27,7 @@ class TestMessageSteps:
     text = "Hii You look great. We would love to promote you on our Page. Are you interested? Please direct message at @v1_usr. Give my reference 'dh'. Send message 'dh' to @v1_usr.\n"
 
     def test_follow_from_post_likes_url(self, driver, credentials):
+        global like, follow_status, follow_button
         authorised = True
         url = "https://www.instagram.com/p/CrQcpOwPOYg/?img_index=1"
 
@@ -40,33 +41,46 @@ class TestMessageSteps:
             sleep(2)
 
             likes = follow_flow.follow_from_post_likes(url)
-            iteration_count = 0
+            total_count = 1
+            iteration_count = 1
 
             for like in range(1, likes):
                 sleep(2)
+                "/html/body/div[6]/div[1]/div/div[2]/div/div/div/div/div/div[2]/div/div/div[1]/div/div/div/div[3]/div/button"
+                "/html/body/div[6]/div[1]/div/div[2]/div/div/div/div/div/div[2]/div/div/div[11]/div/div/div/div[3]/div/button"
+                index = like
                 follow_path = f"/html/body/div[6]/div[1]/div/div[2]/div/div/div/div/div/div[2]/div/div/div[{like}]/div/div/div/div[3]/div/button"
+                follow_secondary_path = f"/html/body/div[6]/div[1]/div/div[2]/div/div/div/div/div/div[2]/div/div/div[9]/div/div/div/div[3]/div/button"
+                if total_count > 8:
+                    follow_path = "/html/body/div[6]/div[1]/div/div[2]/div/div/div/div/div/div[2]/div/div/div[9]/div/div/div/div[3]/div/button"
                 element_locator = (By.XPATH, follow_path)
-                follow_button = WebDriverWait(driver, 10).until(
-                    EC.visibility_of_element_located(element_locator)
-                )
-                follow_status = follow_button.text
+                element_secondary_locator = (By.XPATH, follow_secondary_path)
+                try:
+                    follow_button = WebDriverWait(driver, 10).until(
+                        EC.visibility_of_element_located(element_locator))
+                    follow_status = follow_button.text
+                except Exception:
+                    follow_secondary_button = WebDriverWait(driver, 10).until(
+                        EC.visibility_of_element_located(element_secondary_locator))
+                    follow_secondary_button.send_keys(Keys.ARROW_DOWN, Keys.ARROW_DOWN)
 
-                if follow_status == "Following":
+                if follow_status == "Following" or follow_status == "Requested":
                     follow_button.send_keys(Keys.ARROW_DOWN, Keys.ARROW_DOWN)
-                    pass
+                    total_count += 1
+
                 else:
                     follow_button.click()
-                    follow_button.send_keys(Keys.ARROW_DOWN, Keys.ARROW_DOWN)
                     iteration_count += 1
+                    total_count += 1
+                    follow_button.send_keys(Keys.ARROW_DOWN, Keys.ARROW_DOWN)
+                    sleep_duration = random.uniform(1, 5)
+                    sleep(sleep_duration)
 
-                sleep_duration = random.uniform(1, 5)
-                sleep(sleep_duration)
                 if iteration_count % 8 == 0:
                     print("Sleeping for 1 hour...")
                     sleep(3600)
                     print("Resuming actions...")
-
-
+                    print("followed ", like, "accounts")
 
         else:
             before_sign_page.execute_login(credentials)
